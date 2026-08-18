ac4cdee8d061e0e65f860ce2ea2b3d79e7a990b2
szczun <szcisek@student.agh.edu.pl>
2026-08-18 22:57:05 +0200

    feat: add route and handler for profile creation

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -5,3 +5,7 @@ import "net/http"
 func (app *application) home(w http.ResponseWriter, r *http.Request) {
 	app.render(w, http.StatusOK, "home.html")
 }
+
+func (app *application) userCreate(w http.ResponseWriter, r *http.Request) {
+	app.render(w, http.StatusOK, "createUser.html")
+}
--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -9,6 +9,8 @@ func (app *application) routes() http.Handler {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/{$}", app.home)
 	mux.Handle("/static/", http.StripPrefix("/static/", fs))
+	mux.HandleFunc("GET /create/user", app.userCreate)
+	//mux.HandleFunc("POST /user/create", app.userCreatePost)
 
 	return app.serverRecover(secureHeaders(mux))
 }
